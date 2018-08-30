b43b4b2d02f9f77944cfd7e74ce289b7495b4f9b
Jeffrey Webb <jeff@MacBook-Pro.localdomain>
2018-08-30 17:43:47 -0400

    movie posters appear on home

--- src/App.js
+++ src/App.js
@@ -5,35 +5,10 @@ import NowShowingList from "./Components/NowShowingList";
 import MovieDetails from "./Components/MovieDetails";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        results: [{}]
-      }
-    };
-  }
-
-  componentDidMount() {
-    const URL =
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=3d986795ed5bf93d949ce7ee0258c436&language=en-US&page=1";
-    fetch(URL)
-      .then(resp => {
-        if (resp.status === 200) {
-          return resp.json();
-        } else {
-          // handle error
-        }
-      })
-      .then(json => {
-        this.setState({
-          data: json
-        });
-      });
-  }
+  
 
   render() {
-    console.log(this.state.data.results[0].title);
+    // console.log(this.state.data.results[0].title);
     return (
       <Router>
         <section className="main">
--- src/Components/NowShowingList.js
+++ src/Components/NowShowingList.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class NowShowingList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        results: [{}]
+      }
+    };
+  }
+
+
+  componentDidMount() {
+    const URL =
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=3d986795ed5bf93d949ce7ee0258c436&language=en-US&page=1";
+    fetch(URL)
+      .then(resp => {
+        if (resp.status === 200) {
+          return resp.json();
+        } else {
+          // handle error
+        }
+      })
+      .then(json => {
+        this.setState({
+          data: json
+        });
+      });
+  }
   render() {
+    console.log(this.state.data.results)
     return (
       <section>
-          <Link to={'./hello'}>
-        <h1> Now Showing Jeff Webb </h1>
+      {this.state.data.results.map((movie, i ) => {
+        return (
+        <Link to={`./${movie.id}`} key={i}>
+          <h1>{movie.id}</h1>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
         </Link>
-      </section>
-    );
+        );
+
+        })}
+        </section>
+    )
   }
 }
 
