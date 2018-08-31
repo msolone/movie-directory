8cd682c5f04c517f7a9a12bc56345f10299fb1e6
Michael Solone <michaelsolone@gmail.com>
2018-08-31 00:25:28 -0400

    Added details to MovieDetails component and sticky header and footer

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import NowShowingList from "./Components/NowShowingList";
 import MovieDetails from "./Components/MovieDetails";
 
 class App extends Component {
-  
-
   render() {
     // console.log(this.state.data.results[0].title);
     return (
       <Router>
         <section className="main">
-          <section className="header">Header</section>
-          <section className='body'>
-          <Switch>
-            <Route path="/" exact component={NowShowingList} />
-            <Route path="/:id" exact component={MovieDetails} />
-          </Switch>
+          <section className="header">
+            <h2>NOW SHOWING IN A THEATRE NEAR YOU!</h2>
+          </section>
+          <section className="body">
+            <Switch>
+              <Route path="/" exact component={NowShowingList} />
+              <Route path="/:id" exact component={MovieDetails} />
+            </Switch>
+          </section>
+          <section className="footer">
+            <h3>Created by Jeff Webb and Michael Solone</h3>
           </section>
         </section>
       </Router>
--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieData: {},
+      movieId: this.props.match.params.id,
+      movieCast: {
+          cast: [{}]
+      }
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            movieData: {},
-            movieId: this.props.match.params.id
+  componentDidMount() {
+    const movieURL = `https://api.themoviedb.org/3/movie/${
+      this.state.movieId
+    }?api_key=3d986795ed5bf93d949ce7ee0258c436&language=en-US&page=1`;
+    fetch(movieURL)
+      .then(resp => {
+        if (resp.status === 200) {
+          return resp.json();
+        } else {
+          // handle error
         }
-    }
-    
+      })
+      .then(json => {
+        this.setState({
+          movieData: json
+        });
+      });
 
-    componentDidMount() {
-    
-        const URL =
-        `https://api.themoviedb.org/3/movie/${this.state.movieId}?api_key=3d986795ed5bf93d949ce7ee0258c436&language=en-US&page=1`;
-      fetch(URL)
+      const castURL = `https://api.themoviedb.org/3/movie/${
+        this.state.movieId
+      }/credits?api_key=3d986795ed5bf93d949ce7ee0258c436&language=en-US&page=1`;
+      fetch(castURL)
         .then(resp => {
           if (resp.status === 200) {
             return resp.json();
@@ -25,20 +43,33 @@ class MovieDetails extends Component {
         })
         .then(json => {
           this.setState({
-            movieData: json
+            movieCast: json
           });
         });
+  }
 
-    }
-
-    
-    render() {
-        return (
-            <h1>
-                Mike Solone
-            </h1>
-        );
-    }
+  render() {
+    console.log(this.state.movieData);
+    console.log(this.state.movieCast);
+    return (
+      <section className="movie-details">
+        <section>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${
+              this.state.movieData.poster_path
+            }`}
+            alt={this.state.movieData.poster_path}
+          />
+        </section>
+        <section className="movie-details-info">
+        <h1>{this.state.movieData.title}</h1>
+        <h2>{this.state.movieData.overview}</h2>
+        <h3>{this.state.movieCast.cast[0].name} as {this.state.movieCast.cast[0].character}</h3>
+        <h3>Run Time: {this.state.movieData.runtime} mins</h3>
+        </section>
+      </section>
+    );
+  }
 }
 
 export default MovieDetails;
