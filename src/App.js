96a065afc1d62e1452ef4dd15bfac10e559606a8
Michael Solone <michaelsolone@gmail.com>
2018-08-31 21:23:56 -0400

    Added CSS to header, footer and movie details page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import MediaChoice from "./Components/MediaChoice";
 import MediaList from "./Components/MediaList";
-import MediaDetails from "./Components/MediaDetails"
-
+import MediaDetails from "./Components/MediaDetails";
 
 class App extends Component {
   render() {
@@ -12,7 +11,23 @@ class App extends Component {
       <Router>
         <section className="main">
           <section className="header">
-            <h2>NOW SHOWING IN A THEATRE NEAR YOU!</h2>
+            <section className="logo">
+              <a href="http://localhost:3000">
+                <h2>JMDb</h2>
+              </a>
+            </section>
+            <form className="form-wrapper">
+              <input type="text" placeholder="Search here..." />
+              <button type="submit">SEARCH</button>
+            </form>
+            <section className="header-links">
+              <a href="http://localhost:3000/movie">
+                <h1>Movies</h1>
+              </a>
+              <a href="http://localhost:3000/tv">
+                <h1>TV Shows</h1>
+              </a>
+            </section>
           </section>
           <section className="body">
             <Switch>
